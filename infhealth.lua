e2853ea916e7ec44cfe58b7c233ce42cb67a106d
polswert1 <123364939+Piesel22@users.noreply.github.com>
2023-04-29 11:06:35 +0200

    Update infhealth.lua

--- infhealth.lua
+++ infhealth.lua
@@ -3,6 +3,6 @@ local player = players.LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character.Humanoid
 while true do
-	humanoid.Health = 100
-	wait(0.1)
+	humanoid.Health = humanoid.MaxHealth
+	wait(0.01)
 end
